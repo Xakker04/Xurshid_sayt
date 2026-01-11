b8891560008cc86ef4230bba477b0e2a055a4cca
Xakker04 <157778986+Xakker04@users.noreply.github.com>
2026-01-11 23:28:48 +0500

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,10 @@
+function showInfo() {
+    document.getElementById("extraInfo").innerHTML =
+        "Multimedia texnologiyalari kafedrasi Muhammad al-Xorazmiy nomidagi " +
+        "TATU Nukus filiali Kengashining 2023 yil 15-avgustdagi 453-sonli " +
+        "buyrug‘iga asosan tashkil etilgan. " +
+        "O‘zbekiston Respublikasi Prezidentining 2025 yil 24-yanvardagi " +
+        "PQ-25-son \"Nukus davlat texnika universitetini tashkil etish " +
+        "to‘g‘risida\" qarori asosida qayta tuzildi va " +
+        "Televizion texnologiyalar kafedrasi bo‘lib tashkil etildi.";
+}
